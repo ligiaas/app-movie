b95de690f54f67f94da6704fbdf97c8e2a55d64a
Ligia de Almeida Silva <ligiaalmeidasilva@gmail.com>
2019-06-20 19:27:27 -0300

    add HTTP request.
    adicionando as chamadas GET pro TMDB, ainda não finalizado.
    Removendo o in18.json, não tá sendo usado.

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,21 +1,38 @@
 import React from 'react';
+import * as api from './api/index';
 import './App.css';
 
-import { img } from './assets/img/aggretsuko.jpg';
-import { data } from './api/in18.json'; 
+// import { data } from './api/in18.json'; 
 
 
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgData: img,
-      imgAlt: data.movie.title
+      error: null,
+      isLoaded: false,
+      img: '',
+      movies: []
     };
   }
 
   componentDidMount() {
-    
+    api.getMovies()
+      .then(
+        (result) => {
+          console.log((result));
+          this.setState({
+            isLoaded: true,
+            movies: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   componentWillUnmount() {
@@ -23,17 +40,27 @@ class Carousel extends React.Component {
   }
   
   render() {
-    return (
-      <div>
-        <div className="carousel slide">
-          <div className="carousel-inner">
-            <div className="carousel-item">
-              <img src={this.state.imgData} alt={this.state.imgAlt} className="d-block w-100" />
+    const {error, isLoaded, movies} = this.state;
+    if(error) {
+      return <div>Error: {error.message}</div>;
+    } else if(!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <div className="container">
+            <div className="row">
+              {movies.map((item, index) => (
+                <div className="col-12" key={item.data.results[index].id}>
+                  <p>{item.data.results[index].title}</p>
+                  {/* <img src={`https://image.tmdb.org/t/p/w154${item.data.poster_path}`} alt={item.data.title} className="d-block w-100" /> */}
+                </div>
+              ))}
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,11 @@
-import axios from 'axions';
+import axios from 'axios';
 
-const rootApi = 'https://api.themoviedb.org/3';
+const rootApi = 'https://api.themoviedb.org/3/discover/movie';
 const api_key = '9b3b5f58a3aa91db5dca0d71820a7321';
+var languages = ['pt-BR', 'en-US'];
+var param = 'popularity.desc';
 
 export function getMovies() {
-  const movie = axios.get(`${rootApi}/movie/550?api_key=${api_key}`);
-  const tv = axios.get(`${rootApi}/movie/550?api_key=${api_key}`);
+  const movie = axios.get(`${rootApi}?api_key=${api_key}&language=${languages[0]}&sort_by=${param}`);
+  return Promise.all([movie]);
 }
-
-
-// /movie/550?api_key=
