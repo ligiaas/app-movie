cade13cfc7122f1c6fc93681e6f016fe4dc6a548
Ligia de Almeida Silva <ligiaalmeidasilva@gmail.com>
2019-06-21 01:57:01 -0300

    ajustes css
    ajuste de posicionamento elementos navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 // import Clock from './App';
 // import Carousel from './Carousel';
 import Navbar from './Navbar';
+import './App.css';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './webmovies.png';
 import './App.css';
 
 class Navbar extends React.Component {
@@ -24,10 +23,13 @@ class Navbar extends React.Component {
   render() {
     // const {error, isLoaded, movies} = this.state;
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="collapse navbar-collapse" id="navbar">
-          <span className="navbar-text mr-auto wm_brand">Web Movies</span>
-          <form className="form-inline my-2 my-lg-0">
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        <span className="navbar-text wm_brand">Web Movies</span>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navDrop" aria-controls="navDrop" aria-expanded="false" aria-label="Navegação Mobile">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navDrop">
+          <form className="form-inline ml-auto my-2 my-lg-0">
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
             <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
           </form>
