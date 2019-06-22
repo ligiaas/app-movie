d700869c8b03dc168804490cbb849f974971d248
Ligia de Almeida Silva <ligiaalmeidasilva@gmail.com>
2019-06-22 12:57:12 -0300

    Criado MovieRecord.js
    Criado o comp de ficha do filme.

---
+++ src/components/MovieRecord.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../assets/App.css';
+
+class Movie extends React.Component {
+  
+  render() {
+    const record = this.props.item;
+      return (
+        <div>
+          <div className="card mb-3">
+            <div className="row no-gutters">
+              <div className="col-md-4">
+                <img src="https://via.placeholder.com/550" className="card-img" alt="Title movie"/>
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">{record.title} <small className="text-muted">2019</small></h5>
+                  <p className="card-text">
+                    Avaliação dos usuários
+                  </p>
+                  <h3>Sinopse</h3>
+                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p className="card-text"><small className="text-muted">Warner Movies</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+  }
+}
+
+export default Movie;
