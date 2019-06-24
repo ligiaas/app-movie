1f8d1674263cf1cf7272f34356f48407103f3760
Ligia de Almeida Silva <ligiaalmeidasilva@gmail.com>
2019-06-24 04:40:52 -0300

    Criado o omp Result.js
    Criado o componente Result.js

---
+++ src/components/Result.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Search from './Search';
+
+class Result extends React.Component {
+  // var value = this.props.
+}
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,14 +4,12 @@ import '../assets/App.css';
 class Search extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange() {
-    // eslint-disable-next-line no-restricted-globals
-    this.setState({value: event.target.value});
+  handleChange(e) {
+    this.props.onSearchTextChange(e.target.value);
   }
 
   handleSubmit(event) {
@@ -22,7 +20,7 @@ class Search extends React.Component {
   render() {
     return (
       <form className="form-inline ml-auto my-2 my-lg-0" onSubmit={this.handleSubmit}>
-        <input className="form-control mr-sm-2" type="search" placeholder="Pesquisar..." aria-label="Pesquisar" value={this.state.value} onChange={this.handleChange}/>
+        <input className="form-control mr-sm-2" type="search" placeholder="Pesquisar..." aria-label="Pesquisar" value={this.props.searchText} onChange={this.handleChange}/>
         <input className="btn btn-outline-light my-2 my-sm-0" type="submit" value="Pesquisar"/>
       </form>
     );
