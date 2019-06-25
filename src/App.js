498635c94911dc5b9093a48550c0057db158f304
Ligia de Almeida Silva <ligiaalmeidasilva@gmail.com>
2019-06-25 02:42:33 -0300

    Instalado React Router
    Instalada a biblioteca de rotas do React.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './components/Home';
+import Result from './components/Result';
+import MovieRecord from './components/MovieRecord';
 import './assets/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/result" component={Result} />
+        <Route path="/movie" component={MovieRecord} />
+      </div>
+    </Router>
   );
 }
 
